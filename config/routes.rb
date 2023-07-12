fdd767bc1025404f41513fa66c900be7a59f0bee
EC2 Default User <ec2-user@ip-172-31-14-164.ap-northeast-1.compute.internal>
2023-07-12 07:40:18 +0000

    customer1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,51 @@
 Rails.application.routes.draw do
+  
+  namespace :public do
+    get 'customers/edit'
+    get 'customers/quit'
+    get 'customers/show'
+  end
+# 顧客用
+# URL /customers/sign_in ...
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+ # 顧客用.
+     scope module: :public do
+      root to: "homes#top"
+        get 'about' => 'homes#about'
+        get "search" => "searches#search"
+        
+      resources :items, only: [:index, :show]
+
+        get 'customers/my_page' => 'customers#show'                          #顧客のマイページ.
+        get 'customers/information/edit' => 'customers#edit'                 #顧客の登録情報編集画面.
+        patch 'customers/information' => 'customers#update'                  #顧客の登録情報更新.
+        get 'customers/quit' => 'customers#quit'                             #顧客の退会確認画面.
+        patch 'customers/withdraw' => 'customers#withdraw'       
+      end
+# ゲスト
+devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
+# 管理者用.
+      namespace :admin do
+        get '/' => "homes#top"
+
+      resources :tags, only: [:index, :create, :edit, :update]
+      resources :customers, only:  [:index, :show, :edit, :update]
+     
+
+     end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
